7f9cc7afabae7bcdbac255a53f670994c1d6a3b0
Elliana May <me@mause.me>
2026-04-01 17:32:11 +0800

    labels

--- rbconfig-patch.rb
+++ rbconfig-patch.rb
@@ -5,8 +5,8 @@ puts "Original hdrdir: #{$hdrdir}"
 RbConfig::CONFIG["rubyhdrdir"] = File.join("@TERMUX_PREFIX@", "include", "ruby-#{RbConfig::CONFIG['ruby_version']}")
 $hdrdir = RbConfig::CONFIG["rubyhdrdir"]
 puts "Using rubyhdrdir=#{$hdrdir}"
-puts Dir.children(File.join("@TERMUX_PREFIX@", "include"))
-puts Dir.children($hdrdir)
+puts "include children: #{Dir.children(File.join("@TERMUX_PREFIX@", "include"))}"
+puts "hdrdir children: #{Dir.children($hdrdir)}"
 $target = $hdrdir + "/ruby.h"
 if not File.exist?($hdrdir + "/ruby.h")
   abort "#{$hdrdir}/ruby.h does not exist"

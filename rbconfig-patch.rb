6fc7a64891d51836733846475e1e313845cb9ace
Elliana May <me@mause.me>
2026-04-01 17:30:16 +0800

    dir include

--- rbconfig-patch.rb
+++ rbconfig-patch.rb
@@ -5,6 +5,7 @@ puts "Original hdrdir: #{$hdrdir}"
 RbConfig::CONFIG["rubyhdrdir"] = File.join("@TERMUX_PREFIX@", "include", "ruby-#{RbConfig::CONFIG['ruby_version']}")
 $hdrdir = RbConfig::CONFIG["rubyhdrdir"]
 puts "Using rubyhdrdir=#{$hdrdir}"
+puts Dir.children(File.join("@TERMUX_PREFIX@", "include"))
 puts Dir.children($hdrdir)
 $target = $hdrdir + "/ruby.h"
 if not File.exist?($hdrdir + "/ruby.h")

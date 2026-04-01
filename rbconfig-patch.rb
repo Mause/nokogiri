0b53134eedcb1276733e87b3e6e3af63cf024683
Elliana May <me@mause.me>
2026-04-01 17:23:43 +0800

    retarget

--- rbconfig-patch.rb
+++ rbconfig-patch.rb
@@ -5,6 +5,7 @@ puts "Original hdrdir: #{$hdrdir}"
 RbConfig::CONFIG["rubyhdrdir"] = File.join("@TERMUX_PREFIX@", "include", "ruby-#{RbConfig::CONFIG['ruby_version']}")
 $hdrdir = RbConfig::CONFIG["rubyhdrdir"]
 puts "Using rubyhdrdir=#{$hdrdir}"
-if not File.exist?($hdrdir + "/ruby/ruby.h")
-  abort "#{$hdrdir}/ruby/ruby.h does not exist"
+$target = $hdrdir + "/ruby.h"
+if not File.exist?($hdrdir + "/ruby.h")
+  abort "#{$hdrdir}/ruby.h does not exist"
 end

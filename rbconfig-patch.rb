54a1955ccff999e87ff4fb17c27e9cc97defce9a
Elliana May <me@mause.me>
2026-04-01 17:23:52 +0800

    ls children

--- rbconfig-patch.rb
+++ rbconfig-patch.rb
@@ -2,6 +2,7 @@ require 'rbconfig'
 
 $hdrdir = RbConfig::CONFIG["rubyhdrdir"]
 puts "Original hdrdir: #{$hdrdir}"
+puts Dir.children("@TERMUX_PREFIX@")
 RbConfig::CONFIG["rubyhdrdir"] = File.join("@TERMUX_PREFIX@", "include", "ruby-#{RbConfig::CONFIG['ruby_version']}")
 $hdrdir = RbConfig::CONFIG["rubyhdrdir"]
 puts "Using rubyhdrdir=#{$hdrdir}"

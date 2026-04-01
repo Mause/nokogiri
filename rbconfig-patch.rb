a497845d8ea2658a2933a64d727625d3e793dd6e
Elliana May <me@mause.me>
2026-04-01 17:10:37 +0800

    fix var ref

--- rbconfig-patch.rb
+++ rbconfig-patch.rb
@@ -1,7 +1,8 @@
 require 'rbconfig'
 
 RbConfig::CONFIG["rubyhdrdir"] = File.join("@TERMUX_PREFIX@", "include", "ruby-#{RbConfig::CONFIG['ruby_version']}")
-puts "Using rubyhdrdir=#{RbConfig::CONFIG['rubyhdrdir']}"
-if not File.exist?(($hdrdir = RbConfig::CONFIG["rubyhdrdir"]) + "/ruby/ruby.h")
-  abort "#{hdrdir}/ruby/ruby.h does not exist"
+$hdrdir = RbConfig::CONFIG["rubyhdrdir"]
+puts "Using rubyhdrdir=#{$hdrdir}"
+if not File.exist?($hdrdir + "/ruby/ruby.h")
+  abort "#{$hdrdir}/ruby/ruby.h does not exist"
 end
